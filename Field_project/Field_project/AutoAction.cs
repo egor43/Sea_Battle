0dcec5905b801502ec580764fb14b74bce5c5365
egor43 <egor43.rus@mail.ru>
2017-04-29 11:23:51 +0300

    +

---
+++ Field_project/Field_project/AutoAction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Field_project
+{
+    class AutoAction
+    {
+        private int dimension = 0; // Размерность поля
+
+        public AutoAction (int dimension)
+        {
+            if(0<= dimension)
+            {
+                this.dimension = dimension;
+            }
+            else
+            {
+                dimension = 0;
+                throw new ArgumentException("Передана неверная размерность");
+            }
+        }
+
+        public void AutoSetShips(ref Unit[,] matrix_state)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Field_project/Field_project/Utilits.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Field_project
+{
+    static class Utilits
+    {
+       
+        //Проверка корректности расположения корабля
+        public static bool Check_Ship(Point[] points, Unit[,] state, byte count_value_ship)
+        {
+            if (points.Length != count_value_ship) return false;
+            if (count_value_ship == 1) return (Check_Ship_One(points, state));
+            byte tmp = Sort_Points(points);
+
+            if (tmp != 0)
+            {
+                if (tmp == 2)//Вертикально
+                {
+                    if ((points[0].X + count_value_ship - 1) != (points[points.Length - 1].X)) return false; //Если корабля не имеет разрывов по оси X
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        double I = points[i].X;
+                        double J = points[i].Y;
+
+                        if (i == 0)// для верхней части корабля
+                        {
+                            try
+                            {
+                                if (state[(int)I, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false; //Если соседние ячейки не пустые
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I - 1, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I - 1, (int)J].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I - 1, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                        }
+                        if (i != 0 && i != points.Length - 1)// для средних части корабля
+                        {
+                            try
+                            {
+                                if (state[(int)I, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false; //Если соседние ячейки не пустые
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                        }
+                        if (i == points.Length - 1)// для нижней части корабля
+                        {
+                            try
+                            {
+                                if (state[(int)I, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false; //Если соседние ячейки не пустые
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I + 1, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I + 1, (int)J].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I + 1, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                        }
+                    }
+                }
+                else//Горизонтально
+                {
+                    if ((points[0].Y + count_value_ship - 1) != (points[points.Length - 1].Y)) return false; //Если корабля не имеет разрывов по оси Y
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        double I = points[i].X;
+                        double J = points[i].Y;
+
+                        if (i == 0)// для левой части корабля
+                        {
+                            try
+                            {
+                                if (state[(int)I - 1, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false; //Если соседние ячейки не пустые
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I + 1, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I + 1, (int)J].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I - 1, (int)J].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                        }
+                        if (i != 0 && i != points.Length - 1)// для средней части корабля
+                        {
+                            try
+                            {
+                                if (state[(int)I - 1, (int)J].Get_Unit_Type() != unit_type.sea) return false; //Если соседние ячейки не пустые
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I + 1, (int)J].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                        }
+                        if (i == points.Length - 1)// для правой части корабля
+                        {
+                            try
+                            {
+                                if (state[(int)I - 1, (int)J].Get_Unit_Type() != unit_type.sea) return false; //Если соседние ячейки не пустые
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I - 1, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I + 1, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                            try
+                            {
+                                if (state[(int)I + 1, (int)J].Get_Unit_Type() != unit_type.sea) return false;
+                            }
+                            catch { }
+                        }
+                    }
+                }
+            }
+            else return false;
+
+            return true;
+        }
+
+        //Проверка корректности установки однопалубного корабля
+        private static bool Check_Ship_One(Point[] points, Unit[,] state)
+        {
+            double I = points[0].X;
+            double J = points[0].Y;
+
+            try
+            {
+                if (state[(int)I, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false;
+            }
+            catch { }
+            try
+            {
+                if (state[(int)I - 1, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false;
+            }
+            catch { }
+            try
+            {
+                if (state[(int)I - 1, (int)J].Get_Unit_Type() != unit_type.sea) return false;
+            }
+            catch { }
+            try
+            {
+                if (state[(int)I - 1, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+            }
+            catch { }
+            try
+            {
+                if (state[(int)I, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+            }
+            catch { }
+            try
+            {
+                if (state[(int)I + 1, (int)J - 1].Get_Unit_Type() != unit_type.sea) return false;
+            }
+            catch { }
+            try
+            {
+                if (state[(int)I + 1, (int)J].Get_Unit_Type() != unit_type.sea) return false;
+            }
+            catch { }
+            try
+            {
+                if (state[(int)I + 1, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+            }
+            catch { }
+            try
+            {
+                if (state[(int)I, (int)J + 1].Get_Unit_Type() != unit_type.sea) return false;
+            }
+            catch { }
+
+            return true;
+        }
+
+        // Cортирует массив входящих точек для дальнейшей обработки в методе проверки расположения точек
+        private static byte Sort_Points(Point[] points)
+        {
+            byte checktmp = 0; //переменная для определения ориентации корабля
+            Point tmp = new Point();
+
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                if (points[i].X == points[i + 1].X) checktmp++;
+            }
+
+            if (checktmp == points.Length - 1)//Если прокатило с Х
+            {
+                for (int j = 0; j < points.Length - 1; j++)
+                    for (int i = 0; i < points.Length - 1 - j; i++)
+                    {
+                        if (points[i].Y > points[i + 1].Y)
+                        {
+                            tmp = points[i];
+                            points[i] = points[i + 1];
+                            points[i + 1] = tmp;
+                        }
+                    }
+                //сортируем по X
+                return 1; //Горизонтально
+            }
+            else
+            {
+                checktmp = 0;
+                for (int i = 0; i < points.Length - 1; i++)
+                {
+                    if (points[i].Y == points[i + 1].Y) checktmp++;
+                }
+                if (checktmp == points.Length - 1)//Если прокатило с Y
+                {
+                    for (int j = 0; j < points.Length - 1; j++)
+                        for (int i = 0; i < points.Length - 1 - j; i++)
+                        {
+                            if (points[i].X > points[i + 1].X)
+                            {
+                                tmp = points[i];
+                                points[i] = points[i + 1];
+                                points[i + 1] = tmp;
+                            }
+                        }
+                    //сортируем по Y
+                    return 2;//Вертикально
+                }
+                else return 0;//Если не прокатило совсем
+            }
+        }
+    }
+}
